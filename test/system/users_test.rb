b71db590e0357a967f107df13021d30dd4dc29e6
colorbox <colorbox222@gmail.com>
2018-10-12 23:22:47 +0900

    Other version of click input button

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -34,6 +34,20 @@ class UsersTest < ApplicationSystemTestCase
     click_on "Back"
   end
 
+  test "creating a User 2" do
+    visit users_url
+    click_on "New User"
+
+    input = find('input#user_name')
+    input.set(@user.name)
+
+    submit_button = find('input[value="Create User"]')
+    submit_button.click
+
+    assert_text "User was successfully created"
+    click_on "Back"
+  end
+
   test "updating a User" do
     visit users_url
     click_on "Edit", match: :first
