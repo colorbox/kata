4b4b86401544765142d9b02b1c6b32effdb158a7
colorbox <colorbox222@gmail.com>
2018-10-14 22:39:50 +0900

    find_button

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -61,6 +61,20 @@ class UsersTest < ApplicationSystemTestCase
     click_on "Back"
   end
 
+  test "find_button" do
+    visit users_url
+    click_on "New User"
+
+    input = find('input#user_name')
+    input.set(@user.name)
+
+    button = find_button(value:'Create User')
+    button.click
+
+    assert_text "User was successfully created"
+    click_on "Back"
+  end
+
   test "updating a User" do
     visit users_url
     click_on "Edit", match: :first
