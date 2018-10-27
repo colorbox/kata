d812b7133f5eb6398ebcddcaf47a62d7a5d20217
colorbox <colorbox222@gmail.com>
2018-10-27 23:09:09 +0900

    :lipstick:

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -1,4 +1,4 @@
-require "application_system_test_case"
+require 'application_system_test_case'
 
 class UsersTest < ApplicationSystemTestCase
 
@@ -16,40 +16,40 @@ class UsersTest < ApplicationSystemTestCase
     row = find(:row, 3)
   end
 
-  test "visiting the index" do
+  test 'visiting the index' do
     visit users_url
-    assert_selector "h1", text: "Users"
+    assert_selector 'h1', text: 'Users'
   end
 
-  test "visiting the index2" do
+  test 'visiting the index2' do
     visit users_url
-    click_on "New User"
+    click_on 'New User'
 
     field = find('input#user_name').ancestor('.field')
   end
 
-  test "sibiling" do
+  test 'sibiling' do
     visit users_url
     sib2 = find('.sib1').sibling('.sib2')
     sib3 = find('.sib1').sibling('.sib3')
   end
 
-  test "creating a User" do
+  test 'creating a User' do
     visit users_url
-    click_on "New User"
+    click_on 'New User'
 
     input = find('input#user_name')
     input.set(@user.name)
 
-    click_on "Create User"
+    click_on 'Create User'
 
-    assert_text "User was successfully created"
-    click_on "Back"
+    assert_text 'User was successfully created'
+    click_on 'Back'
   end
 
-  test "creating a User 2" do
+  test 'creating a User 2' do
     visit users_url
-    click_on "New User"
+    click_on 'New User'
 
     input = find('input#user_name')
     input.set(@user.name)
@@ -57,11 +57,11 @@ class UsersTest < ApplicationSystemTestCase
     submit_button = find('input[value="Create User"]')
     submit_button.click
 
-    assert_text "User was successfully created"
-    click_on "Back"
+    assert_text 'User was successfully created'
+    click_on 'Back'
   end
 
-  test "find_button find_link" do
+  test 'find_button find_link' do
     visit users_url
 
     new_user_link = find_link(text: 'New User')
@@ -73,19 +73,19 @@ class UsersTest < ApplicationSystemTestCase
     button = find_button(value:'Create User')
     button.click
 
-    assert_text "User was successfully created"
-    click_on "Back"
+    assert_text 'User was successfully created'
+    click_on 'Back'
   end
 
-  test "find_by_id" do
+  test 'find_by_id' do
     visit users_url
 
     find_by_id('id_ans')
   end
 
-  test "find_field" do
+  test 'find_field' do
     visit users_url
-    click_on "New User"
+    click_on 'New User'
 
     find('.field')
     find_field()
@@ -93,26 +93,26 @@ class UsersTest < ApplicationSystemTestCase
     find_field('Name')
   end
 
-  test "updating a User" do
+  test 'updating a User' do
     visit users_url
-    click_on "Edit", match: :first
+    click_on 'Edit', match: :first
 
-    click_on "Update User"
+    click_on 'Update User'
 
-    assert_text "User was successfully updated"
-    click_on "Back"
+    assert_text 'User was successfully updated'
+    click_on 'Back'
   end
 
-  test "destroying a User" do
+  test 'destroying a User' do
     visit users_url
     page.accept_confirm do
-      click_on "Destroy", match: :first
+      click_on 'Destroy', match: :first
     end
 
-    assert_text "User was successfully destroyed"
+    assert_text 'User was successfully destroyed'
   end
 
-  test "checked? check" do
+  test 'checked? check' do
     visit users_url
 
     assert_equal(false, find('input#page_freezeflag').checked?)
@@ -127,7 +127,7 @@ class UsersTest < ApplicationSystemTestCase
     assert_equal(false, find('input#page_freezeflag').checked?)
   end
 
-  test "choose" do
+  test 'choose' do
     visit users_url
 
     rb = find('#radio_category_socrates')
