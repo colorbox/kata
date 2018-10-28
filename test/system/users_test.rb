3fd665a95f01feeb91713f632dfd5e35dcd312ba
colorbox <colorbox222@gmail.com>
2018-10-28 21:37:46 +0900

    update test order

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -10,28 +10,9 @@ class UsersTest < ApplicationSystemTestCase
     @user = users(:one)
   end
 
-  test 'add_selector' do
-    visit users_url
-
-    row = find(:row, 3)
-  end
-
   test 'visiting the index' do
     visit users_url
     assert_selector 'h1', text: 'Users'
-  end
-
-  test 'visiting the index2' do
-    visit users_url
-    click_on 'New User'
-
-    field = find('input#user_name').ancestor('.field')
-  end
-
-  test 'sibiling' do
-    visit users_url
-    sib2 = find('.sib1').sibling('.sib2')
-    sib3 = find('.sib1').sibling('.sib3')
   end
 
   test 'creating a User' do
@@ -45,6 +26,44 @@ class UsersTest < ApplicationSystemTestCase
 
     assert_text 'User was successfully created'
     click_on 'Back'
+  end
+
+  test 'updating a User' do
+    visit users_url
+    click_on 'Edit', match: :first
+
+    click_on 'Update User'
+
+    assert_text 'User was successfully updated'
+    click_on 'Back'
+  end
+
+  test 'destroying a User' do
+    visit users_url
+    page.accept_confirm do
+      click_on 'Destroy', match: :first
+    end
+
+    assert_text 'User was successfully destroyed'
+  end
+
+  test 'add_selector' do
+    visit users_url
+
+    row = find(:row, 3)
+  end
+
+  test 'visiting the index2' do
+    visit users_url
+    click_on 'New User'
+
+    field = find('input#user_name').ancestor('.field')
+  end
+
+  test 'sibiling' do
+    visit users_url
+    sib2 = find('.sib1').sibling('.sib2')
+    sib3 = find('.sib1').sibling('.sib3')
   end
 
   test 'creating a User 2' do
@@ -88,28 +107,8 @@ class UsersTest < ApplicationSystemTestCase
     click_on 'New User'
 
     find('.field')
-    find_field()
     find_field(name: 'user[name]')
     find_field('Name')
-  end
-
-  test 'updating a User' do
-    visit users_url
-    click_on 'Edit', match: :first
-
-    click_on 'Update User'
-
-    assert_text 'User was successfully updated'
-    click_on 'Back'
-  end
-
-  test 'destroying a User' do
-    visit users_url
-    page.accept_confirm do
-      click_on 'Destroy', match: :first
-    end
-
-    assert_text 'User was successfully destroyed'
   end
 
   test 'checked? check' do
