d68011065835d7f70019309f41f813f9359bac7c
colorbox <colorbox222@gmail.com>
2018-11-13 00:49:55 +0900

    Use active_decorator and show_for

--- Gemfile
+++ Gemfile
@@ -39,7 +39,7 @@ gem 'bootsnap', '>= 1.1.0', require: false
 
 gem 'active_decorator'
 # gem 'draper'
-# gem 'show_for'
+gem 'show_for'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,16 +1,15 @@
-class UserDecorator < Draper::Decorator
-  delegate_all
+module UserDecorator
+  # def created_at
+  #   helpers.content_tag :span, class: 'time' do
+  #     object.created_at.strftime("%a %m/%d/%y")
+  #   end
+  # end
 
-  # Define presentation-specific methods here. Helpers are accessed through
-  # `helpers` (aka `h`). You can override attributes, for example:
-  #
-  def created_at
-    helpers.content_tag :span, class: 'time' do
-      object.created_at.strftime("%a %m/%d/%y")
-    end
+  def trial_created_at
+    created_at.strftime("%a %m/%d/%y")
   end
 
-  def trial_name
-    "trial! #{name}"
+def trial_name
+    "trial!!!! #{name}"
   end
 end
